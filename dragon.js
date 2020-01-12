30863279f31e6d981e505715e2e9ad6a1e7b84c4
Nicolas S <nsepulveda92@gmail.com>
2020-01-12 17:26:35 -0300

    second upload of master

--- dragon.js
+++ dragon.js
@@ -1,16 +1,36 @@
+const TRAITS = require('./traits')
+
 const DEFAULT_PROPERTIES = {
- nickname: 'unanmed',
- get birthdate () {
-     return new Date()
- }
+    nickname: 'not assigned', 
+    get birthdate(){
+        return new Date()
+    },
+
+    get randomTraits() {
+        const traits = [];
+ 
+        TRAITS.forEach(TRAIT => {
+            const traitType = TRAIT.type;
+            const traitValues = TRAIT.values;
+
+            const traitValue = traitValues[
+                Math.floor(Math.random() * traitValues.length) 
+            ];
+
+            traits.push({ traitType, traitValue})
+        });
+    
+        return traits;
+    }
 }
 // sets default propertioes for objects (dragon in this case)
 // Does it set it for all objects?
 
 class Dragon {
-    constructor ({birthdate, nickname} = {}) {
+    constructor ({birthdate, nickname, traits} = {}) {
         this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
         this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
+        this.traits = traits || DEFAULT_PROPERTIES.randomTraits;
     }
 }
 
--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-const Dragon = require('./dragon');
+// const Dragon = require('./dragon');
 
-const fooey = new Dragon ( {birthdate: new Date(), nickname: 'fooey'})
-const baloo = new Dragon ({birthdate: new Date(), nickname: 'baloo'})
+// const fooey = new Dragon();
+// const baloo = new Dragon();
+// const mimar = new Dragon();
 
-const mimar = new Dragon();
-const gooby = new Dragon();
+// setTimeout(() => {
+//     const gooby = new Dragon();
+//     console.log('gooby', gooby);
+// }, 3000);
 
-console.log('fooey', fooey);
-console.log('baloo', baloo);
 
-console.log('mimar', mimar);
-console.log('gooby', gooby);
+
+// console.log('fooey', fooey);
+// console.log('baloo', baloo);
+// console.log('mimar', mimar);
+
+// const Generation = require ('./generation');
+
+// const generation = new Generation();
+
+// console.log('generation', generation);
+
+// const gooby = generation.newDragon();
+
+// console.log('gooby', gooby)
+
+// setTimeout(() => {
+//     const mimar = generation.newDragon();
+//     console.log('mimar', mimar);
+// }, 15000)
+
+const GenerationEngine = require ('./engine');
+
+const engine = new GenerationEngine();
+
+engine.start();
+
+setTimeout(() => {
+    engine.stop();
+}, 20000);
