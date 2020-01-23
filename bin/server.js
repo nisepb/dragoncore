32aae212b406f1d8efe3a2306c34c2090db43081
Nicolas S <nsepulveda92@gmail.com>
2020-01-23 12:48:36 -0300

    path updates

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,5 @@
-const app ( require('../app/index'))
+const app = require('../app');
 
 const port = 3000;
 
-app.listen(port, () => console.log('listening on port ${port}'));
+app.listen(port, () => console.log(`listening on port ${port}`));
